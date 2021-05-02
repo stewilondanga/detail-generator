2a65ae5055a0090b9ba89843aa334701e26f7965
Stewart ilondanga <stilondanga@gmail.com>
2021-05-02 11:37:03 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-/*var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+var navigate = (function() {
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
-
+/*
 var main = document.getElementById("main");
 var generator = document.getElementById("generator");
 var gallery = document.getElementById("gallery");
